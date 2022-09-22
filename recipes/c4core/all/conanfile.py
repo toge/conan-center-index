e750a9adb68fc77512016792897a9a9ea744e4aa
toge <toge.mail@gmail.com>
2022-09-22 23:21:40 +0900

    use conan.tools.files.copy
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/c4core/all/conanfile.py
+++ recipes/c4core/all/conanfile.py
@@ -81,7 +81,7 @@ class C4CoreConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE*", dst="licenses", src=self.source_folder)
+        copy(self, pattern="LICENSE*", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "cmake"))
